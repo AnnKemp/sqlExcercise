fdb9f0d33408e58b0d2ad1908c9775261ccd0711
AnnKemp <ann.kemp@scarlet.be>
2020-02-25 12:13:18 +0100

    first sketch of the profile page

--- profile.php
+++ profile.php
@@ -1,1 +1,44 @@
 <?php
+
+include("connection.php");
+// -------------------------------------- this is en VIEW-page ------------------------------------------------------------
+echo "<!DOCTYPE html>
+<html>
+<head>
+<style>
+section {
+    margin-left: auto;
+    margin-right:auto;
+    text-align: center;
+  font-family: arial, sans-serif;
+  width: 50%;
+  height: 80%;
+  border:solid 1px grey;
+  
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>";
+//<h1 href='profile.php?user=" . $row["id"]
+
+try {
+    foreach ($conn->query('SELECT * from student') as $row) {
+        echo "<section><h1>" . $row["firstname"]." ".$row["lastname"] . "</h1><p>" . $row["email"] . "</p><em>" . $row["quote"] . "</em><br /><small>" . $row["quote_author"] . "</small></section>";
+    }
+    $conn = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+echo "</body></html>";
+?>
