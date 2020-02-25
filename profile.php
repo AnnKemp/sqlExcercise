5649fb58c566d2a31bdafd11cd73c163868d5136
AnnKemp <ann.kemp@scarlet.be>
2020-02-25 15:15:31 +0100

    recent version

--- profile.php
+++ profile.php
@@ -1,6 +1,8 @@
 <?php
 
 include("connection.php");
+$id=$_GET['user'];
+// echo "Dit is de : $id"; // this test works! :)
 // -------------------------------------- this is en VIEW-page ------------------------------------------------------------
 echo "<!DOCTYPE html>
 <html>
@@ -9,30 +11,32 @@ echo "<!DOCTYPE html>
 section {
     margin-left: auto;
     margin-right:auto;
+    margin-top:40px;
+    padding-top: 300px;
     text-align: center;
   font-family: arial, sans-serif;
-  width: 50%;
-  height: 80%;
-  border:solid 1px grey;
+  width: 25%;
+  height: 200px;
   
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  background-image:url('assets/img/woods.jpg');
+  background-repeat: no-repeat;
+  background-position: top;
+ 
 }
 
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
-
-tr:nth-child(even) {
-  background-color: #dddddd;
+h1 {
+  margin-top:0%;
 }
 </style>
 </head>
 <body>";
 //<h1 href='profile.php?user=" . $row["id"]
+//$sql = "SELECT id, firstname, lastname FROM MyGuests WHERE lastname='Doe'";
+//$result = $conn->query($sql);
 
 try {
-    foreach ($conn->query('SELECT * from student') as $row) {
+    foreach ($conn->query('SELECT id, firstname, lastname, email, quote, quote_author FROM student WHERE id=3') as $row) {
         echo "<section><h1>" . $row["firstname"]." ".$row["lastname"] . "</h1><p>" . $row["email"] . "</p><em>" . $row["quote"] . "</em><br /><small>" . $row["quote_author"] . "</small></section>";
     }
     $conn = null;
