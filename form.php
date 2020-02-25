1a267aedb2c03aa75d4414a7627a8865bf7fae3b
AnnKemp <ann.kemp@scarlet.be>
2020-02-25 12:09:42 +0100

    latest version of the form

--- form.php
+++ form.php
@@ -35,71 +35,70 @@
                 <div class="form-group col-md-6">
                     <label for="firstname">First name:</label>
                     <!-- https://www.w3schools.com/php/php_form_complete.asp -->
-                    <input type="text" name="firstname" id="firstname" class="form-control" value="<?php echo $firstname;?>" >
+                    <input type="text" name="firstname" id="firstname" class="form-control" required value="<?php echo $firstname;?>" >
                     <span class="error"> <?php echo $streetErr; ?></span>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="lastname">last name:</label>
-                    <input type="text" id=lastname" name="lastname" class="form-control" value="<?php echo $lastname;?>">
+                    <input type="text" id=lastname" name="lastname" class="form-control" required value="<?php echo $lastname;?>">
                     <span class="error"> <?php echo $streetnrErr; ?></span>
                 </div>
             </div>
             <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="username">Username</label>
-                        <input type="text" id="username" name="username" class="form-control" value="<?php echo $username;?>">
+                        <input type="text" id="username" name="username" class="form-control" required value="<?php echo $username;?>">
                         <span class="error"> <?php echo $zipcodeErr; ?></span>
                     </div>
                 <div class="form-group col-md-6">
                     <label for="email">E-mail:</label>
-                    <input type="text" id="email" name="email" class="form-control" value="<?php echo $email; ?>" />
+                    <input type="text" id="email" name="email" class="form-control" required value="<?php echo $email; ?>" />
                     <span class="error"> <?php echo $emailErr; ?></span>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="github">Github:</label>
-                    <input type="text" id="github" name="github" class="form-control" value="<?php echo $email; ?>" />
+                    <input type="text" id="github" name="github" class="form-control" required value="<?php echo $email; ?>" />
                     <span class="error"> <?php echo $emailErr; ?></span>
                 </div>
 
                 <div class="form-group col-md-6">
                     <label for="linkedin">LinkedIn:</label>
-                    <input type="text" id="linkedin" name="linkedin" class="form-control" value="<?php echo $city;?>">
+                    <input type="text" id="linkedin" name="linkedin" class="form-control" required value="<?php echo $city;?>">
                     <span class="error"> <?php echo $cityErr; ?></span>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="lang">Prefered language:</label>
-                    <input type="text" id="lang" name="lang" class="form-control" value="<?php echo $email; ?>" />
+                    <input type="text" id="lang" name="lang" class="form-control" required value="<?php echo $email; ?>" />
                     <span class="error"> <?php echo $emailErr; ?></span>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="lang">Video link:</label>
-                    <input type="text" id="video" name="video" class="form-control" value="<?php echo $email; ?>" />
+                    <input type="text" id="video" name="video" class="form-control" required value="<?php echo $email; ?>" />
                     <span class="error"> <?php echo $emailErr; ?></span>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="github">Quote:</label>
-                    <input type="text" id="quote" name="quote" class="form-control" value="<?php echo $email; ?>" />
+                    <input type="text" id="quote" name="quote" class="form-control" required value="<?php echo $email; ?>" />
                     <span class="error"> <?php echo $emailErr; ?></span>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="quote_author">Author quote:</label>
-                    <input type="text" id="quote_author" name="quote_author" class="form-control" value="<?php echo $email; ?>" />
+                    <input type="text" id="quote_author" name="quote_author" class="form-control" required value="<?php echo $email; ?>" />
                     <span class="error"> <?php echo $emailErr; ?></span>
                 </div>
             </div>
-            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="text" name="fileToUpload" required id="fileToUpload">
         </fieldset>
         <p>
         <button type="submit" class="btn btn-primary">SEND</button>
         </p>
     </form>
-
     <footer>Fuck off! You already ordered food and drinks.</footer>
 </div>
 </body>
