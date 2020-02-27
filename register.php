7bf5f07ea64a0901fbd5e932c8efba36d12ebc19
AnnKemp <ann.kemp@scarlet.be>
2020-02-27 12:52:41 +0100

    latest version

--- register.php
+++ register.php
@@ -4,42 +4,26 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-include("connection.php");
 
-try {
-    $sql = $conn->prepare("INSERT INTO student (firstname, lastname, username, linkedin, github, email, preferredlanguage, avatar, video, quote, quote_author) VALUES (:firstname, :lastname, :username, :linkedin, :github, :email, :preferredlanguage, :avatar, :video, :quote, :quote_author)");
-    $sql->bindParam(':firstname', $firstname);
-    $sql->bindParam(':lastname', $lastname);
-    $sql->bindParam(':username', $username);
-    $sql->bindParam(':linkedin', $linkedin);
-    $sql->bindParam(':github', $github);
-    $sql->bindParam(':email', $email);
-    $sql->bindParam(':preferredlanguage', $lang);
-    $sql->bindParam(':avatar', $avatar);
-    $sql->bindParam(':video', $video);
-    $sql->bindParam(':quote', $quote);
-    $sql->bindParam(':quote_author', $quote_author);
+include("Model/Database.php");
 
-    // insert a row
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $github = $_POST['github'];
-    $linkedin = $_POST['linkedin'];
-    $lang = $_POST['lang'];
-    $video = $_POST['video'];
-    $quote = $_POST['quote'];
-    $quote_author = $_POST['quote_author'];
-    $avatar=$_POST['fileToUpload'];
-    $sql->execute();
-
-    echo "New record created successfully";
-}
-catch(PDOException $e)
-{
-    echo $sql . "<br>" . $e->getMessage();
-}
 $conn = null;
+
+
+//$sql = "SELECT firstname, lastname, email, preferredlanguage, linkedin FROM student";
+// kan nog niet want nog geen waarden in student
+//$stmt = $db->prepare('SELECT * FROM student');
+//$stmt->bindValue(PDO::PARAM_INT);
+//$stmt->bindValue(PDO::PARAM_STR);
+//$stmt->execute();
+
+//$statement = $pdo->query("SELECT * FROM student");
+//$row = $statement->fetch(PDO::FETCH_ASSOC);
+//echo htmlentities($row['*']);
+//echo "<ul>";
+//foreach ($statement as $rij) {
+// echo "<li>".$rij['firstname']." - ".$rij['lastname']."</li>";
+//}
+//echo "</ul>";
 
 ?>
