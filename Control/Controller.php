a34f67f5b2681d810902808be94eaa9e90ba0fd1
AnnKemp <ann.kemp@scarlet.be>
2020-02-27 12:50:54 +0100

    still working on it but here you have an intermediate stadium

--- Control/Controller.php
+++ Control/Controller.php
@@ -13,27 +13,42 @@ class Controller
             $user = $show_rows->show_alldata();
 
             if (isset($_POST['firstname'])) {
-                // add data to database  // elke keer als ik refresh in de browser voegt ie die laatste data opnieuw toe  dus ik moet ergens mijn $_Post terug leegmaken
+                // add data to database
 
                 $data_insert = new Database();
-                $data_insert->insert($_POST['firstname'], $_POST['lastname'], $_POST['username'], $_POST['linkedin'], $_POST['github'], $_POST['email'], $_POST['lang'], $_POST['fileToUpload'], $_POST['video'], $_POST['quote'], $_POST['quote_author']);
+                $data_insert->insert($_POST['firstname'], $_POST['lastname'], $_POST['username'], $_POST['linkedin'], $_POST['github'], $_POST['email'], $_POST['lang'], $_POST['fileToUpload'], $_POST['video'], $_POST['quote'], $_POST['quote_author'], $_POST['password']);
 
-                $_POST['firstname']=$_POST['lastname']=$_POST['username']=$_POST['linkedin']=$_POST['github']=$_POST['email']=$_POST['lang']=$_POST['fileToUpload']=$_POST['video']=$_POST['quote']=$_POST['quote_author']="";
+// make the posts empty
+$_POST['firstname']=$_POST['lastname']=$_POST['username']=$_POST['linkedin']=$_POST['github']=$_POST['email']=$_POST['lang']=$_POST['fileToUpload']=$_POST['video']=$_POST['quote']=$_POST['quote_author']=$_POST['password']="";
+
+ // add validation to form   -  making the object
+$data_validatie=new Validation();
+
+ // making the array to check if empty
+$toValidate = array($_POST['firstname'], $_POST['lastname'], $_POST['username'], $_POST['linkedin'],  $_POST['github'], $_POST['email'], $_POST['lang'], $_POST['fileToUpload'], $_POST['video'], $_POST['quote'], $_POST['quote_author']);
+
+// die $errom met [1] gebruiken om in de view error aan te duiden wanneer iets leeg is moet dat nog doen
+$errom=$data_validatie->ifempty($toValidate);
+//print_r($errom);  // this is still empty
             }
         }
-            // add validation to form   -  making the object
-    /*        $data_validatie=new Validation();
-            // making the array to validate
-            $toValidate = array($_POST['firstname'], $_POST['lastname'], $_POST['username'], $_POST['linkedin'],  $_POST['github'], $_POST['email'], $_POST['lang'], $_POST['fileToUpload'], $_POST['video'], $_POST['quote'], $_POST['quote_author']);
-
-            // die errom met [1] gebruiken om in de view error aan te duiden wanneer iets leeg is
-            $errom=$data_validatie->ifempty($toValidate);
-            // geef terug $errom[0] is empty  om aan te duiden dat een veld leeg is
-
-            // deze waarden nog opvangen om
-            $data_validatie->validate($_POST['firstname'], $_POST['lastname'], $_POST['username'], $_POST['linkedin'],  $_POST['github'], $_POST['email'], $_POST['lang'], $_POST['fileToUpload'], $_POST['video'], $_POST['quote'], $_POST['quote_author']);
+// deze waarden nog opvangen om
+     /*       $data_validatie->validate($_POST['firstname'], $_POST['lastname'], $_POST['username'], $_POST['linkedin'],  $_POST['github'], $_POST['email'], $_POST['lang'], $_POST['fileToUpload'], $_POST['video'], $_POST['quote'], $_POST['quote_author']);
         }*/
         //load the view
         require 'View/homepage.php';
-    }
+
+//----------------------------- for the profile page ------------------------------------------------------
+        if($_SERVER["REQUEST_METHOD"] == "GET"){  // nog eens checken ik moet testen of link geklikt da's eerder javascript
+
+            $id=$_GET['user'];
+
+            $show_profile = new Database();
+            $show_profile->get_data_for_profile($this->id);
+            $userprofile = $show_profile->show_userdata();
+
+            require "View/profile.php";
+                }
+        }
+
 }
